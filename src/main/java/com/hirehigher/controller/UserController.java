6170188e21ad45c213f77389787e4e71b459ef55
jming623 <jming623@naver.com>
2021-07-12 14:35:20 +0900

    유저기능작업 브랜치 생성

---
+++ src/main/java/com/hirehigher/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.hirehigher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping("/userLogin")
+	public void userLogin() {
+		
+	}
+	
+}
