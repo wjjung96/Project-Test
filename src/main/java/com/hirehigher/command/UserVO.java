4fad66275b91aaf3855c1230398ade30299d2711
wjjung96 <hiu03013@naver.com>
2021-07-13 17:25:43 +0900

    test

--- src/main/java/com/hirehigher/command/UserVO.java
+++ src/main/java/com/hirehigher/command/UserVO.java
@@ -1,5 +1,7 @@
 package com.hirehigher.command;
 
+import java.security.Timestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,15 @@ import lombok.NoArgsConstructor;
 public class UserVO {
 	
 	private String userId;
+	private String userPw;
+	private String userName;
+	private String nickName;
+	private String birthDay;
+	private String userEmail;
+	private String userCellNum;
+	private String addrZipNum;
+	private String addrBasic;
+	private String addDetail;
+	private Timestamp regdate;
+	private int userType;
 }
